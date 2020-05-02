a04a9485d63af07c3c31974c8d22e15c5e5efc03
Nikita <mykyta.tokariev@gmail.com>
2020-05-02 13:12:05 +0300

    Finished deleting users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,4 +40,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect user delete when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect user delete to root url when not admin" do
+    post_login_path(@another_user.email)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    post_login_path(@another_user.email)
+    assert_not @another_user.admin?
+    patch user_path(@another_user), params: {
+        user: {
+            password: "password",
+            password_confirmation: "password",
+            admin: true
+        }
+    }
+    assert_not @another_user.reload.admin?
+  end
+
+  test "delete user" do
+    post_login_path(@user.email)
+    assert_difference 'User.count', -1 do
+      delete user_path(@another_user)
+    end
+  end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,14 +3,33 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:alice)
+    @admin = users(:alice)
+    @non_admin = users(:bob)
   end
 
-  test "index including pagination" do
-    post_login_path(@user.email)
+  test "index as admin including pagination and delete links" do
+    post_login_path(@admin.email)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
+
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'Delete'
+      end
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    post_login_path(@non_admin)
+    get users_path
+    assert_select 'a', text: "Delete", count: 0
   end
 
 end
