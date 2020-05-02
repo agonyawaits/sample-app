b69d3fc526347bd997c55974830721ff498a6c90
Nikita <38644295+agonyawaits@users.noreply.github.com>
2020-05-02 13:31:21 +0300

    Finished deleting users (#9)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '~> 2.7'
+ruby '~> 2.7.1'
 
 gem 'rails',                    '~> 6.0', '>= 6.0.2.1'
 gem 'pg',                       '~> 1.2', '>= 1.2.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Nikita Tokariev",
+             email: "mykyta.tokariev@gmail.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
 50.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
