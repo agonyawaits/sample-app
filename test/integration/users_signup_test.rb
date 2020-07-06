962b67d876d1b4e3264a78e6545143c712e9a01b
Nikita <38644295+agonyawaits@users.noreply.github.com>
2020-07-06 12:11:12 +0300

    Finished account activation (#14)
    * Finished account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
+    get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {
         user: {
@@ -20,7 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {
@@ -33,6 +38,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
 
+    assert_equal(1, ActionMailer::Base.deliveries.size, 'One email should have been sent')
+    user = assigns(:user)
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    # Valid activation token, valid email
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
