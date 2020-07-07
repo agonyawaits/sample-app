c71176d0a4fc02e1da0945bbef00fb5016b314e1
Mykyta Tokariev <mykyta.tokariev@gmail.com>
2020-07-07 13:33:20 +0300

    User activation via email (SendGrid) fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://fathomless-ridge-95540.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+
   ActionMailer::Base.smtp_settings = {
+      :user_name => 'apikey',
+      :password => ENV['SENDGRID_APIKEY'],
+      :api_key => ENV['SENDGRID_APIKEY'],
+      :domain => 'heroku.com',
       :address => 'smtp.sendgrid.net',
       :port => '587',
       :authentication => :plain,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'heroku.com',
       :enable_starttls_auto => true
   }
 
