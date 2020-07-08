8d71e58b3be003152d6abbfd374e97c576f294a3
Mykyta Tokariev <mykyta.tokariev@gmail.com>
2020-07-08 23:14:58 +0300

    Finished microposts base

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,9 @@ User.create!(
       activated: true,
       activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |u| u.microposts.create!(content: content) }
+end
